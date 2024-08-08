38715af36c00fc05c25d42f59c274157cd12353a
Harold <98804534+HaroldVisperas@users.noreply.github.com>
2024-08-09 00:58:40 +0800

    Integrated frontend fixes for brand creation and brand profile

--- resources/views/company/company-brandcreation.blade.php
+++ resources/views/company/company-brandcreation.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Brand Details</title>
+    <title>ROCPH Digital Marketing Services</title>
     <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
@@ -13,9 +13,9 @@
     <link rel="stylesheet" href="{{ asset('assets/css/company-brand-create.css') }}">
 </head>
 
-<body class="maxwidth">
+<body>
     <!-- NavBar -->
-    <nav class="navbar navbar-expand-lg blue borderbottom {border-bottom: white 5px solid;}">
+    <nav class="navbar navbar-expand-lg blue borderbottom fixed-top {border-bottom: white 5px solid;}">
         <div class="container-fluid">
             <!-- offcanvas trigger -->
             <button class="navbar-toggler navbar-dark" type="button" data-bs-toggle="offcanvas"
@@ -35,11 +35,11 @@
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item dropdown ">
                         <div class="d-flex align-items-center my-2">
-                        <div class="rounded-4 me-3"
-                            style="width: 50px; height: 50px; background-image: url('{{ asset('assets/images/icon.png') }}'); background-size: cover;">
-                        </div>
+                            <div class="rounded-4 me-3"
+                            style="width: 50px; height: 50px; background-image: url('{{ asset('assets/images/Default.png') }}'); background-size: cover;">
+                            </div>
                             <div>
-                                <div class="text-white fs-2 fw-bold text-uppercase">{{ auth()->user()->firstname }} {{ auth()->user()->middlename }} {{ auth()->user()->lastname }}</div>
+                            <div class="text-white fs-2 fw-bold text-uppercase">{{ auth()->user()->firstname }} {{ auth()->user()->middlename }} {{ auth()->user()->lastname }}</div>
                                 <div class="text-white fs-6 text-end neg10">
                                     @if (auth()->user()->role == 'company_owner')
                                         Company Owner Account
@@ -53,10 +53,10 @@
                             <li><a class="dropdown-item" href="#">My Profile</a></li>
                         </ul>
                     </li>
-                    <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown"
+                    <a class="navbarName dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown"
                         aria-expanded="True"></a>
                     <ul class="dropdown-menu dropdown-menu-end">
-                        <form id="profile-form" method="GET" action="{{ route('company.profile.create') }}">
+                    <form id="profile-form" method="GET" action="{{ route('company.profile.create') }}">
                             @csrf
                             <li><a class="dropdown-item" href="#" onclick="event.preventDefault(); document.getElementById('profile-form').submit();">
                                 <i class="bi bi-gear fs-5 me-2"></i>My Profile</a></li>
@@ -73,10 +73,12 @@
         </div>
     </nav>
     <!-- NavBar -->
-    <div class="container-fluid p-0 maxwidth">
-        <div class="row stripe borderbottom orange">i</div>
-        <div class="row stripe borderbottom red">i</div>
-        <div class="row stripe borderbottom green">i</div>
+    <div class="container-fluid">
+        <div class="row stripes">
+            <div class="col-12 stripe borderbottom orange"></div>
+            <div class="col-12 stripe borderbottom red"></div>
+            <div class="col-12 stripe borderbottom green"></div>
+        </div>
     </div>
     <!-- SideBar -->
     <div class="offcanvas offcanvas-start blue text-white sidebar-nav " tabindex="-1" id="offcanvasExample"
@@ -84,117 +86,124 @@
         <div class="offcanvas-body">
             <nav class="navbar-dark">
                 <ul class="navbar-nav">
-                    <form id="dashboard-link" method="GET" action="{{ route('company.dashboard.create') }}">
-                        @csrf
-                        <a href="#" class="nav-link text-white text-start pt-3" onclick="event.preventDefault(); document.getElementById('dashboard-link').submit();">
-                            <i class="bi bi-microsoft fs-5 me-2"></i>
-                            <span class="text-uppercase fw-bold fs-5">Dashboard</span>
-                        </a>
-                    </form>
-                    <form id="brand-create-link" method="GET" action="{{ route('company.brand.create') }}">
-                        @csrf
-                        <a href="#" class="nav-link text-white text-start pt-3" onclick="event.preventDefault(); document.getElementById('brand-create-link').submit();">
-                            <i class="bi bi-building-fill-add fs-5 me-2"></i>
-                            <span class="text-uppercase fw-bold fs-5">Create Brand</span>
-                        </a>
-                    </form>
-                    <form id="brands-link" method="GET" action="{{ route('company.brands.create') }}">
-                        @csrf
-                        <a href="#" class="nav-link text-white text-start pt-3" onclick="event.preventDefault(); document.getElementById('brands-link').submit();">
-                            <i class="bi bi-bag-heart-fill fs-5 me-2"></i>
-                            <span class="text-uppercase fw-bold fs-5">Brands</span>
-                        </a>
-                    </form>
-                    <form id="employees-link" method="GET" action="{{ route('company.employee.create') }}">
-                        @csrf
-                        <input type="hidden" name="email" value="{{ auth()->user()->email }}">
-                        <a href="#" class="nav-link text-white text-start pt-3" onclick="event.preventDefault(); document.getElementById('employees-link').submit();">
-                            <i class="bi bi-person-hearts fs-5 me-2"></i>
-                            <span class="text-uppercase fw-bold fs-5">Employees</span>
-                        </a>
-                    </form>
+                    <li>
+                        <form id="dashboard-link" method="GET" action="{{ route('company.dashboard.create') }}">
+                            @csrf
+                            <a href="#" class="nav-link text-white text-start pt-3" onclick="event.preventDefault(); document.getElementById('dashboard-link').submit();">
+                                <i class="bi bi-microsoft fs-5 me-2"></i>
+                                <span class="text-uppercase fw-bold fs-5">Dashboard</span>
+                            </a>
+                        </form>
+                        <form id="brand-create-link" method="GET" action="{{ route('company.brand.create') }}">
+                            @csrf
+                            <a href="#" class="nav-link text-white text-start pt-3" onclick="event.preventDefault(); document.getElementById('brand-create-link').submit();">
+                                <i class="bi bi-building-fill-add fs-5 me-2"></i>
+                                <span class="text-uppercase fw-bold fs-5">Create Brand</span>
+                            </a>
+                        </form>
+                        <form id="brands-link" method="GET" action="{{ route('company.brands.create') }}">
+                            @csrf
+                            <a href="#" class="nav-link text-white text-start pt-3" onclick="event.preventDefault(); document.getElementById('brands-link').submit();">
+                                <i class="bi bi-bag-heart-fill fs-5 me-2"></i>
+                                <span class="text-uppercase fw-bold fs-5">Brands</span>
+                            </a>
+                        </form>
+                        <form id="employees-link" method="GET" action="{{ route('company.employee.create') }}">
+                            @csrf
+                            <input type="hidden" name="email" value="{{ auth()->user()->email }}">
+                            <a href="#" class="nav-link text-white text-start pt-3" onclick="event.preventDefault(); document.getElementById('employees-link').submit();">
+                                <i class="bi bi-person-hearts fs-5 me-2"></i>
+                                <span class="text-uppercase fw-bold fs-5">Employees</span>
+                            </a>
+                        </form>
+                    </li>
                 </ul>
             </nav>
         </div>
     </div>
     <!-- SideBar -->
 
-    <!-- Upload Image -->
-    <main class="mt-5 pt-1 text-start tblack main">
-        <div class="container form-container">
-        <div class="bg-image">
-            <div class="form-overlay">
-            <h2 class="text-center mb-4 custom-heading">Brand Details</h2>
-                @if ($errors->any())
-                    <div class="alert alert-danger">
-                        <ul>
-                            @foreach ($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                            @endforeach
-                        </ul>
-                    </div>
-                @endif
-                <form method="POST" action="{{ route('company.brand.store') }}" >
-                    @csrf
-                    <input type="hidden" name="user_email" value="{{ auth()->user()->email }}">
-                    <div class="mb-3">
-                        <label for="name" class="form-label">Brand Name*</label>
-                        <input type="text" class="form-control custom-input" id="name" name="name" value="{{ old('name') }}" placeholder="Enter brand name" required>
-                    </div>
-                    <div class="mb-4">
-                        <label for="description" class="form-label">Brand Description*</label>
-                        <textarea class="form-control custom-input" id="description" name="description" rows="3" placeholder="Enter brand description" required>{{ old('description') }}</textarea>
-                    </div>
-                    <div class="mb-1">
-                        <label for="social_media_link" class="form-label p-2">Social Media Links:</label>
-                    </div>
-                    <div class="mb-3 align-items-center">
-                        <div class="d-flex">
-                            <p class="p-3">https://www.facebook.com/</p>
-                            <input type="text" class="form-control custom-input p-2" id="social_media_link" name="facebook_link" placeholder="Enter Facebook link">
-                        </div>
-                    </div>
-                    <div class="mb-3 align-items-center">
-                        <div class="d-flex">
-                            <p class="p-3">https://www.linkedin.com/in/</p>
-                            <input type="text" class="form-control custom-input p-2" id="social_media_link" name="linkedin_link" placeholder="Enter LinkedIn link">
-                        </div>
-                    </div>
-                    <div class="mb-3 align-items-center">
-                        <div class="d-flex">
-                            <p class="p-3">https://www.instagram.com/</p>
-                            <input type="text" class="form-control custom-input p-2" id="social_media_link" name="instagram_link" placeholder="Enter Instagram link">
-                        </div>
-                    </div>
-                    <div class="mb-3 align-items-center">
-                        <div class="d-flex">
-                            <p class="p-3">https://www.x.com/</p>
-                            <input type="text" class="form-control custom-input p-2" id="social_media_link" name="x_link" placeholder="Enter X link">
-                        </div>
-                    </div>
-                    <div class="mb-3 align-items-center">
-                        <div class="d-flex">
-                            <p class="p-3">https://www.youtube.com/@</p>
-                            <input type="text" class="form-control custom-input p-2" id="social_media_link" name="youtube_link" placeholder="Enter Youtube link">
-                        </div>
-                    </div>
-                    <div class="mb-3 align-items-center">
-                        <div class="d-flex">
-                            <p class="p-3">https://www.tiktok.com/</p>
-                            <input type="text" class="form-control custom-input p-2" id="social_media_link" name="tiktok_link" placeholder="Enter Tiktok link">
-                        </div>
-                    </div>
-                    <button type="submit" class="btn blue text-white custom-button">Create</button>
-                </form>
-            </div>  
+    <!-- Main Contents -->
+    <main class="mt-3 text-start tblack main" data-bs-spy="noscroll">
+        <div class="container-fluid">
+            <div class="row justify-content-center contents">
+                <div class="bg-image">
+                <div class="form-overlay">
+                    <h2 class="text-center mb-4 custom-heading">Brand Details</h2>
+                        @if ($errors->any())
+                            <div class="alert alert-danger">
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
+                        <form method="POST" action="{{ route('company.brand.store') }}" >
+                            @csrf
+                            <input type="hidden" name="user_email" value="{{ auth()->user()->email }}">
+                            <div class="mb-3">
+                                <label for="name" class="form-label">Brand Name*</label>
+                                <input type="text" class="form-control custom-input" id="name" name="name" value="{{ old('name') }}" placeholder="Enter brand name" required>
+                            </div>
+                            <div class="mb-4">
+                                <label for="description" class="form-label">Brand Description*</label>
+                                <textarea class="form-control custom-input" id="description" name="description" rows="3" placeholder="Enter brand description" required>{{ old('description') }}</textarea>
+                            </div>
+                            <br>
+                            <h2 class="text-center mb-4 custom-heading">Social Media Links</h2>
+                            <div class="mb-3 align-items-center">
+                            <div class="mb-3">
+                                <div class="d-flex flex-column form-group">
+                                    <label class="form-label">Facebook URL:</label>
+                                    <input type="text" class="form-control custom-input" id="facebook_link" name="facebook_link" placeholder="Enter Facebook link">
+                                </div>
+                            </div>
+                            <div class="mb-3">
+                                <div class="d-flex flex-column form-group">
+                                    <label class="form-label">LinkedIn URL:</label>
+                                    <input type="text" class="form-control custom-input" id="linkedin_link" name="linkedin_link" placeholder="Enter LinkedIn link">
+                                </div>
+                            </div>
+                            <div class="mb-3">
+                                <div class="d-flex flex-column form-group">
+                                    <label class="form-label">Instagram URL:</label>
+                                    <input type="text" class="form-control custom-input" id="instagram_link" name="instagram_link" placeholder="Enter Instagram link">
+                                </div>
+                            </div>
+                            <div class="mb-3">
+                                <div class="d-flex flex-column form-group">
+                                    <label class="form-label">X URL:</label>
+                                    <input type="text" class="form-control custom-input" id="x_link" name="x_link" placeholder="Enter X link">
+                                </div>
+                            </div>
+                            <div class="mb-3">
+                                <div class="d-flex flex-column form-group">
+                                    <label class="form-label">YouTube URL:</label>
+                                    <input type="text" class="form-control custom-input" id="youtube_link" name="youtube_link" placeholder="Enter YouTube link">
+                                </div>
+                            </div>
+                            <div class="mb-3">
+                                <div class="d-flex flex-column form-group">
+                                    <label class="form-label">TikTok URL:</label>
+                                    <input type="text" class="form-control custom-input" id="tiktok_link" name="tiktok_link" placeholder="Enter TikTok link">
+                                </div>
+                            </div>
+                            <button type="submit" class="btn blue text-white custom-button">Create</button>
+                        </form>
+                    </div>  
+                </div>
+            </div>
         </div>
     </main>
 
+    <!-- JavaScript -->
     <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@3.0.2/dist/chart.min.js"></script>
     <script src="{{ asset('assets/js/jquery-3.5.1.js') }}"></script>
     <script src="{{ asset('assets/js/jquery.dataTables.min.js') }}"></script>
     <script src="{{ asset('assets/js/dataTables.bootstrap5.min.js') }}"></script>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js@3.0.2/dist/chart.min.js"></script>
+    <script src="{{ asset('assets/js/script.js') }}"></script>
 </body>
 
 </html>
